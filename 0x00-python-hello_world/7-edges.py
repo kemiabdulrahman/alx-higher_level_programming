0b4b7250373fd89ea268d538cf3b3a1ec2b86591
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-01-02 08:28:03 -0800

    Tasks Done

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 = word[-1:-3]
-middle = word[1:9]
+word_last_2 = word[-2:]
+middle = word[1:-1]
 print("First 3 word : {}".format(word_first_3))
 print("Last two words : {}".format(word_last_2))
 print("middle words: {}".format(middle))
