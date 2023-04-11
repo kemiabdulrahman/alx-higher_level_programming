4c06fd46fc51c5ae98e43c90588495fb3c3803ca
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-11 15:15:23 -0700

    ! done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const num = Math.floor(Number(process.argv[2]));
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let row = 0; row < num; row++) {
+    console.log('C is fun');
+  }
+}
