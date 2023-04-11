7de31e7abf3dbe3b83fbcbbbb4d31a8dbede19e5
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-11 15:25:29 -0700

    ! done

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const amount = Math.floor(Number(process.argv[2]));
+if (isNaN(amount)) {
+  console.log('Missing size');
+} else {
+  for (let row = 0; row < amount; row++) {
+    let r = '';
+    for (let column = 0; column < amount; column++) { r += 'X'; }
+    console.log(r);
+  }
+}
