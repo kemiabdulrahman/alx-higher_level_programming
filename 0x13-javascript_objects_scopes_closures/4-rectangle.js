38da29014a5540ff33360a597fac70b8ada2a2ec
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-13 21:01:48 +0100

    Task Done

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const au = this.width;
+    this.width = this.height;
+    this.width = au;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
