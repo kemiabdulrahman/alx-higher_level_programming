4f8a4515583a67268ea63d85d7f22798f528f61d
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-13 22:54:03 +0100

    Task Done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,16 @@
+#!/usr/bin/node
 const dict = require('./101-data').dict;
 const totallist = Object.entries(dict);
 const values = Object.values(dict);
-
 const uniqueValues = [...new Set(values)];
-
 const Ndict = {};
-
 for (const j in uniqueValues) {
   const list = [];
   for (const k in uniqueValues) {
-    if (t) {
-
+    if (totallist[k][1] === uniqueValues[j]) {
+      list.unshift(totallist[k][0]);
     }
   }
+  Ndict[uniqueValues[j]] = list;
 }
+console.log(Ndict);
