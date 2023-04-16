439ff835391eabb20b41d3047f6ab5b88857add1
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-16 11:21:02 +0100

    added

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host = "localhost", username=sys.argv[1], password = sys.argv[2], database=sys.argv[3], port=3306)
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+    cursor.close()
+    database.close()
