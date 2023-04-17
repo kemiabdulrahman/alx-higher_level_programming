0a1e9c8177d819be05501a2dceafbbb6ebd1dfe7
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-17 07:02:35 +0100

    Task Done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,12 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(host = "localhost", username=sys.argv[1], password = sys.argv[2], database=sys.argv[3], port=3306)
+    database = MySQLdb.connect(
+        host="localhost",
+        username=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306)
     cursor = database.cursor()
     cursor.execute("""SELECT * FROM states""")
     rows = cursor.fetchall()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,9 +4,16 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(host = "localhost", username=sys.argv[1], password = sys.argv[2], database=sys.argv[3], port=3306)
+    database = MySQLdb.connect(
+        host="localhost",
+        username=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306)
     cursor = database.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
+    cursor.execute(
+        """SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDE
+        R BY states.id""")
     rows = cursor.fetchall()
     for i in rows:
         print(i)
