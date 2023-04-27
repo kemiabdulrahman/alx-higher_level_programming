cab344ef0d0209b2a9e8d81e7c3c4659cfd341cb
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-28 00:01:16 +0100

    AbdulRahman MAFE

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 A python script that fetches https://alx-intranet-hbtn.io/status
-* Using urllib
+- Using urllib
 
 """
 if __name__ == '__main__':
@@ -13,5 +13,3 @@ if __name__ == '__main__':
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
-
-
