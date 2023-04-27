5faf123dc211f8c07192cc68968e9886609a6dbd
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-27 23:30:44 +0100

    AbdulRahman MAFE

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Scripts that sends a request to a URL passed as an argument
+awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
+
---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script thsat sends a json POST request to URL passed as the first argument
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+
