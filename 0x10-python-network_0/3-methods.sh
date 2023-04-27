eb08a343856551146a32983341e23a0db9b3ac11
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-27 21:40:14 +0100

    Commit message

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all http methods the server will accept
-curl -ls "$1" | grep "Allow:" | cut -d":"-f 2 | cut -c 2- | rev | cut -c 2- | rev
+curl -Is "$1" | grep "Allow:" | cut -d":"-f 2 | cut -c 2- | rev | cut -c 2- | rev
