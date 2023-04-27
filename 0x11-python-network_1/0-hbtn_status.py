f8e2f8adc9ff78ab881694f69fe89701e266b7c1
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-27 23:57:47 +0100

    AbdulRahman MAFE

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ A python script that fetches https://alx-intranet-hbtn.io/status
 * Using urllib
 
 """
-if __name__ = '__main__':
+if __name__ == '__main__':
     import urllib.request
 
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as ri:
         content = ri.read()
-        print("BODY:")
+        print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
