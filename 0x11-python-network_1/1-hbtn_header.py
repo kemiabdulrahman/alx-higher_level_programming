655abd5e0ada4a65cb724debe3e5855128c8dc63
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-04-28 00:11:41 +0100

    AbdulRahman MAFE

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,8 +9,9 @@ import sys
 import urllib.request
 
 if __name__ == '__main__':
-    url = sys.argv[1]
+    url_argument = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    request = urllib.request.Request(url_argument)
+    with urllib.request.urlopen(request) as reply:
+        print(dict(reply.headers).get("X-Request-Id"))
+
