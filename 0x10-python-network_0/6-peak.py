bbd8119a96d76fffbba630af0b0bf4a9f5d03baa
AbdulRahman MAFE <kemimafe11@gmail.com>
2023-05-02 12:58:47 +0100

    AbdulRahman MAFE

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,18 +8,18 @@ def find_peak(list_of_integers):
     if list_of_integers is None or list_of_integers == []:
         return None
     lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
+    h = len(list_of_integers)
+    mid_num = ((h - lo) // 2) + lo
+    mid_num = int(mid_num)
+    if h == 1:
         return list_of_integers[0]
-    if hi == 2:
+    if h == 2:
         return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    if list_of_integers[mid_num] >= list_of_integers[mid_num - 1] and\
+            list_of_integers[mid_num] >= list_of_integers[mid_num + 1]:
+        return list_of_integers[mid_num]
+    if mid_num > 0 and list_of_integers[mid_num] < list_of_integers[mid_num + 1]:
+        return find_peak(list_of_integers[mid_num:])
+    if mid_num > 0 and list_of_integers[mid_num] < list_of_integers[mid_num - 1]:
+        return find_peak(list_of_integers[:mid_num])
 
